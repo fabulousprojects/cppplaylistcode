ff2b93e381677b70a3277be829ac2aeeb580320c
fabulousprojects <97608310+fabulousprojects@users.noreply.github.com>
2023-11-07 19:57:09 +0530

    Two important files of video 2
    These are the 2 files that we created and use in part 2. If you've come here from the youtube channel stay tuned for more video

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    string charactarname = "Jhon";
+    cout<<charactarname<<" was good ";
+    cout<<charactarname<<" was 43 ";
+    cout<<charactarname<<" was not so old";
+}
